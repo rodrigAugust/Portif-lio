d56b5d55758260de97a2e089278027fbe0df7963
rodrigAugust <rodrigo.augusto.florentino@gmai.com>
2022-09-26 08:35:49 -0300

    update.form

--- script.js
+++ script.js
@@ -52,15 +52,4 @@ $(document).ready(function () {
             }
         }
     });
-    function abrir(URL) {
-
-        var width = 150;
-        var height = 250;
-
-        var left = 99;
-        var top = 99;
-
-        window.open(URL, 'janela', 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left + ', scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
-
-    }
 });
