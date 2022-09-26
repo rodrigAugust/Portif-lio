350d888cf1f7c5d5be533c7601962078b251cef8
rodrigAugust <rodrigo.augusto.florentino@gmai.com>
2022-09-25 21:28:54 -0300

    portfolio

--- script.js
+++ script.js
@@ -1,55 +1,66 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
-     $('.scroll-up-btn').click(function(){
-         $('html').animate({scrollTop: 0});
-     });
-
-     var typed = new Typed(".typing", {
-         strings:["Analista", "Desenvolvedor", "Designer"],
-         typeSpeed:100,
-         backSpeed:60,
-         loop:true
-     });
-     var typed = new Typed(".typing-2", {
-        strings:["Analista", "Desenvolvedor", "Designer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
     });
 
-    $('.menu-btn').click(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Analista", "Desenvolvedor", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Analista", "Desenvolvedor", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
     $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPauser: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
             },
-            600:{
-                items:2,
-                nav:false
+            600: {
+                items: 2,
+                nav: false
             },
-            1000:{
-                items:3,
-                nav:false
+            1000: {
+                items: 3,
+                nav: false
             }
         }
     });
+    function abrir(URL) {
+
+        var width = 150;
+        var height = 250;
+
+        var left = 99;
+        var top = 99;
+
+        window.open(URL, 'janela', 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left + ', scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
+
+    }
 });
